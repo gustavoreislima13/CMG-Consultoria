c53e82fc6e41d8e777c44d5684dc334207e43991
gustavoreislima13 <reisanselmo7@gmail.com>
2024-07-01 21:41:13 -0300

    Depoimento

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,14 @@ const header = document.querySelector("[data-header]");
 navToggleBtn.addEventListener("click", function () {
   header.classList.toggle("active");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const testimonialsWrapper = document.querySelector('.testimonials-wrapper');
+
+  testimonialsWrapper.addEventListener('wheel', (event) => {
+    event.preventDefault();
+    testimonialsWrapper.scrollBy({
+      left: event.deltaY < 0 ? -30 : 30
+    });
+  });
+});
